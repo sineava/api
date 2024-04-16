d2bc4fa984bf38df01c16285044ca647c984fbf0
sineava <1690150504@qq.com>
2024-04-16 15:48:17 +0800

    通过写入文件共享变量,vercel太不人性化了

--- api/index.js
+++ api/index.js
@@ -1,17 +1,20 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 
-let status = false
-
-app.get('/status', (_, res) => {
-  return res.json({ status })
+app.get('/status', async (_, res) => {
+  const data = await fs.readFileSync('./variable.json', 'utf8')
+  const json = JSON.parse(data)
+  return res.json({ status: json.status })
 })
 
-app.get('/change', (req, res) => {
-  status = req.query.status || false
-  return res.send('success')
+app.get('/change', async (req, res) => {
+  const status = req.query?.status === 'true'
+  const json = JSON.stringify({ status })
+  await fs.writeFileSync('./variable.json', json ,'utf8')
+  return res.json({ status: 'success' })
 })
 
-app.listen(3000, () => console.log('Server ready on port 3000.'))
+app.listen(3001, () => console.log('Server ready on port 3001.'))
 
 module.exports = app
