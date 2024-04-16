b9d28acdd7341994481b0c4585af9becd6f9d59c
sineava <1690150504@qq.com>
2024-04-16 15:50:18 +0800

    fix:修复小bug

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 
+// 本地记得加.
 app.get('/status', async (_, res) => {
-  const data = await fs.readFileSync('./variable.json', 'utf8')
+  const data = await fs.readFileSync('/variable.json', 'utf8')
   const json = JSON.parse(data)
   return res.json({ status: json.status })
 })
@@ -11,7 +12,7 @@ app.get('/status', async (_, res) => {
 app.get('/change', async (req, res) => {
   const status = req.query?.status === 'true'
   const json = JSON.stringify({ status })
-  await fs.writeFileSync('./variable.json', json ,'utf8')
+  await fs.writeFileSync('/variable.json', json ,'utf8')
   return res.json({ status: 'success' })
 })
 
