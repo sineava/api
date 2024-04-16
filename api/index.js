e921463c93aa6dcbfc803a81019b4fda3cb1ea0c
sineava <1690150504@qq.com>
2024-04-16 16:43:59 +0800

    fix:api

--- api/index.js
+++ api/index.js
@@ -1,21 +1,16 @@
 const express = require('express')
-const fs = require('fs')
 const app = express()
+
+let status = true
 
 // 本地记得加.
 app.get('/status', async (_, res) => {
-  const data = await fs.readFileSync('/variable.json', 'utf8')
-  const json = JSON.parse(data)
-  return res.json({ status: json.status })
+  return res.json({ status })
 })
 
 app.get('/change', async (req, res) => {
   const status = req.query?.status === 'true'
-  const json = JSON.stringify({ status })
-  await fs.writeFileSync('/variable.json', json ,'utf8')
-  return res.json({ status: 'success' })
+  return res.json({ status })
 })
 
-app.listen(3001, () => console.log('Server ready on port 3001.'))
-
-module.exports = app
+app.listen(3000, () => console.log('Server ready on port 3000'))
